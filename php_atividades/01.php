53f260834c0d5eb0b4fb17ed16d80c58bc1ec0fe
jlemoslemons <wilsonlemos54@gmail.com>
2025-10-13 21:45:05 -0300

    13/10

---
+++ php_atividades/01.php
@@ -0,0 +1,7 @@
+<?php
+// Crie variáveis para armazenar o nome, idade e altura de uma pessoa e exiba-as em uma frase concatenada.
+$nome = "Jessé";
+$idade = "22";
+$altura = "1.85";
+echo $nome . " tem " . $idade . " anos e " . $altura . " de altura.";
+?>
---
+++ php_atividades/02.php
@@ -0,0 +1,4 @@
+<?php
+// Atribua valores de tipos diferentes (string, int, float e bool) e use var_dump() para exibir os tipos.
+
+?>
